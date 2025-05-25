ffeff8509883a4fe25cc2770b93abf60d72c3c3f
JFariaDev <josefaria.webdev@gmail.com>
2025-05-25 23:46:21 +0100

    final

---
+++ Attack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bratalian2
+{
+    public class Attack
+    {
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public int Accuracy { get; set; }
+
+        public Attack(string name, int power, int accuracy)
+        {
+            Name = name;
+            Power = power;
+            Accuracy = accuracy;
+        }
+    }
+}
--- obj/Release/net8.0/Bratalian2.AssemblyInfo.cs
+++ obj/Release/net8.0/Bratalian2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bratalian2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d06ec25b1f2f8983398c45f0fee532f3a5c1355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb2024720f7f539f383aae06a981adcdcfd3979b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bratalian2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bratalian2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
